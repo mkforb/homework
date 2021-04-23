1c7418e53ea68c51c02638d5ffca30cfb013ba67
Alexander <air130@yandex.ru>
2021-04-24 01:25:54 +0300

    homework13 v2

--- src/com/ifmo/jjd/homework13/employee/Employee.java
+++ src/com/ifmo/jjd/homework13/employee/Employee.java
@@ -1,13 +1,48 @@
 package com.ifmo.jjd.homework13.employee;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class Employee {
     private String name;
     private String company;
     private int salary;
     private int age;
+
+    public Employee(String name, String company, int salary, int age) {
+        this.name = name;
+        this.company = company;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", company='" + company + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
 
     // TODO: конструктор, геттеры и сеттеры
 
@@ -16,12 +51,46 @@ public class Employee {
         String[] names = {"Mike", "Tom", "Alex", "John", "Peter", "Jack", "Charlie", "Max", "Jenifer", "Linda", "Elizabeth"}; // массив с именами
         String[] companies = {"Microsoft", "IBM", "Google", "General Electric", "Siemens", "Samsung", "Apple"}; // массив с названиями компаний
 
+        Random random = new Random();
         List<Employee> employees = new ArrayList<>(num);
 
         // добавление num объектов Employee в список (employees)
         for (int i = 0; i < num; i++) {
-            employees.add(new Employee(   )); // TODO: объекты создавать с рандомными значениями. Возраст от 21 до 60 и не забудьте про зп
+            employees.add(new Employee(
+                    names[random.nextInt(names.length)],
+                    companies[random.nextInt(companies.length)],
+                    random.nextInt(30000) + 10000,
+                    random.nextInt(40) + 21
+            )); // TODO: объекты создавать с рандомными значениями. Возраст от 21 до 60 и не забудьте про зп
         }
         return employees;
+    }
+
+    public static class NameComparator implements Comparator<Employee> {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            return o1.name.compareTo(o2.name);
+        }
+    }
+
+    public static class CompanyComparator implements Comparator<Employee> {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            return o1.company.compareTo(o2.company);
+        }
+    }
+
+    public static class SalaryComparator implements Comparator<Employee> {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            return Integer.compare(o1.salary, o2.salary);
+        }
+    }
+
+    public static class AgeComparator implements Comparator<Employee> {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            return Integer.compare(o1.age, o2.age);
+        }
     }
 }
--- src/com/ifmo/jjd/homework13/employee/EmployeeTask.java
+++ src/com/ifmo/jjd/homework13/employee/EmployeeTask.java
@@ -1,6 +1,8 @@
 package com.ifmo.jjd.homework13.employee;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
 
 public class EmployeeTask {
     public static void main(String[] args) {
@@ -10,5 +12,24 @@ public class EmployeeTask {
         // имени
         // имени и зарплате
         // имени, зарплате, возрасту и компании
+        System.out.println(employees);
+        Comparator<Employee> comparator1 = new Employee.NameComparator();
+        TreeSet<Employee> treeSet = new TreeSet<>(comparator1);
+        treeSet.addAll(employees);
+        System.out.println(treeSet);
+        //
+        comparator1 = new Employee.NameComparator()
+                .thenComparing(new Employee.SalaryComparator());
+        treeSet = new TreeSet<>(comparator1);
+        treeSet.addAll(employees);
+        System.out.println(treeSet);
+        //
+        comparator1 = new Employee.NameComparator()
+                .thenComparing(new Employee.SalaryComparator())
+                .thenComparing(new Employee.AgeComparator())
+                .thenComparing(new Employee.CompanyComparator());
+        treeSet = new TreeSet<>(comparator1);
+        treeSet.addAll(employees);
+        System.out.println(treeSet);
     }
 }
