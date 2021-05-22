1f6c1094614b35855dbffecc2a306a86d7dcf45e
Alexander <air130@yandex.ru>
2021-05-22 20:22:05 +0300

    homework26

---
+++ src/com/ifmo/jjd/multithreading/homework26/SyncApp.java
@@ -0,0 +1,7 @@
+package com.ifmo.jjd.multithreading.homework26;
+
+/**
+ * Created by User on 21.05.2021.
+ */
+public class SyncApp {
+}
---
+++ src/com/ifmo/jjd/multithreading/homework26/Transaction.java
@@ -0,0 +1,37 @@
+package com.ifmo.jjd.multithreading.homework26;
+
+public class Transaction implements Runnable{
+    private final Account src;
+    private final Account dst;
+    private int money;
+
+    public Transaction(Account src, Account dst, int money) {
+        this.src = src;
+        this.dst = dst;
+        this.money = money;
+    }
+
+// (account1, account2, 300)
+// (account2, account1, 340)
+    @Override
+    public void run() {
+        // перевод с src на dst
+        /*if (src.getUuid() < dst.getUuid()) {
+            synchronized (src) {
+                synchronized (dst){
+
+                }
+            }
+        } else {
+            synchronized (dst) {
+                synchronized (src){
+
+                }
+            }
+        }*/
+        synchronized (src) {}
+        synchronized (dst) {}
+
+
+    }
+}
